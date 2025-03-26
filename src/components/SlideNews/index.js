91343fbe363fd7db7c069837e187943588786d8f
weilinfox <caiweilin@iscas.ac.cn>
2025-03-26 15:29:27 +0800

    Change slide news to tile
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- src/components/SlideNews/index.js
+++ src/components/SlideNews/index.js
@@ -5,13 +5,6 @@ import { Slide } from "react-slideshow-image";
 
 const slideImages = [
   {
-    title: <Translate>嘉楠勘智K230D</Translate>,
-    subtitle: <Translate>首款基于新32位 RuyiSDK 的AIoT量产芯片</Translate>,
-    Image: "https://images.unsplash.com/photo-1532883031962-d3574f99541b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    Links: "/blog/2024/07/30/k230d",
-    visibility: "secondaryButtonHide",
-  },
-  {
     title: <Translate>RuyiSDK</Translate>,
     subtitle: <Translate>面向RISC-V架构的一体化集成开发环境</Translate>,
     Image: require("@site/static/img/background.jpg").default,
@@ -19,15 +12,29 @@ const slideImages = [
     visibility: "secondaryButtonShow",
     subLinks: "download",
   },
+  {
+    title: <Translate>RuyiSDK 社区</Translate>,
+    subtitle: <Translate>RuyiSDK 社区讨论板块现已开启</Translate>,
+    Image: "https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Links: "https://github.com/ruyisdk/ruyisdk/discussions/",
+    visibility: "secondaryButtonHide",
+  },
+  {
+    title: <Translate>嘉楠勘智K230D</Translate>,
+    subtitle: <Translate>首款基于新32位 RuyiSDK 的AIoT量产芯片</Translate>,
+    Image: "https://images.unsplash.com/photo-1532883031962-d3574f99541b?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    Links: "/blog/2024/07/30/k230d",
+    visibility: "secondaryButtonHide",
+  },
 ];
 
 export default function SlideNews() {
   return (
-    <div className="slide-container">
-      <Slide>
+    <div className={styles.verticalSlideContainer}>
         {slideImages.map((slideImage, index) => (
           <div key={index}>
             <div
+              key={index}
               className={styles.divStyle}
               style={{
                 backgroundImage: `url(${slideImage.Image})`,
@@ -40,6 +47,7 @@ export default function SlideNews() {
                   <a href={slideImage.Links} className={styles.primaryButton}>
                     <Translate id="homepage.primarybutton">了解更多</Translate>
                   </a>
+                  {slideImage.subLinks && (
                   <a
                     href={slideImage.subLinks}
                     className={slideImage.visibility}
@@ -48,12 +56,12 @@ export default function SlideNews() {
                       现在开始
                     </Translate>
                   </a>
+                  )}
                 </div>
               </div>
             </div>
           </div>
         ))}
-      </Slide>
     </div>
   );
 }
