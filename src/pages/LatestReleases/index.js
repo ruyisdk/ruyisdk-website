110c8cd36522dd17051bc56c9336614af8229d01
weilinfox <caiweilin@iscas.ac.cn>
2025-04-28 15:25:13 +0800

    pages: fetch ruyi download link with ruyi backend api
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ src/pages/LatestReleases/index.js
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import CodeBlock from '@theme/CodeBlock';
+
+const ReleaseContext = createContext(null);
+
+export default function ReleaseProvider({ children }) {
+  const [releaseData, setReleaseData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.ruyisdk.cn/releases/latest-pm')
+      .then(res => res.json())
+      .then(data => setReleaseData(data))
+      .catch(err => console.error('API releases/latest-pm 获取版本信息失败', err));
+  }, []);
+
+  return (
+    <ReleaseContext.Provider value={releaseData}>
+      {children}
+    </ReleaseContext.Provider>
+  );
+}
+
+function useReleaseData() {
+  return useContext(ReleaseContext);
+}
+
+export function DownloadRuyi({ arch }) {
+  const data = useReleaseData();
+
+  if (!data) {
+    return <CodeBlock language="bash">$ wget </CodeBlock>;
+  }
+
+  const link = data.channels.stable.download_urls[`linux/${arch}`]?.[1];
+
+  return <CodeBlock language="bash">{`$ wget ${link}`}</CodeBlock>;
+}
+
