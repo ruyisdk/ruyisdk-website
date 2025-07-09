b5b19e059a83a548748fe01030f364dd840a3fec
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-07-10 02:40:18 +0800

    feat:修改了首页RevyOS的链接（英文德语跳转en），其他跳转zh
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "zh-Hans",
+    defaultLocale: "en",
+    // defaultLocale: "zh-Hans",
     locales: ["zh-Hans", "en", "de"],
   },
 
@@ -199,6 +200,7 @@ const config = {
             ],
           },
         ],
+        // links:[],
         copyright: `Copyright © 2024-${new Date().getFullYear()} RuyiSDK`,
       },
       prism: {
