20cb0091d8281b7afb5281de7d8c08c5acb6362d
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-07-10 17:16:48 +0800

    en de页面的二维码也缩小了
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,8 +33,8 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    // defaultLocale: "en",
-    defaultLocale: "zh-Hans",
+    defaultLocale: "en",
+    // defaultLocale: "zh-Hans",
     locales: ["zh-Hans", "en", "de"],
   },
 
