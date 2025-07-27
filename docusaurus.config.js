d02ccb5dbe3b2e2f95b247311222c184f90ada01
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-07-27 17:07:10 +0800

    fix the default language
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,8 +32,7 @@ const config = {
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    // defaultLocale: "zh-Hans",
+    defaultLocale: "zh-Hans",
     locales: ["zh-Hans", "en", "de"],
   },
 
