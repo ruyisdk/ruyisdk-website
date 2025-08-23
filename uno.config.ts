a171f96a25084432089416a5ad9b8ad497369bc8
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2025-08-23 14:00:54 +0800

    refactor: simplifying WeChatLink component with UnoCSS
    Signed-off-by: U2FsdGVkX1 <U2FsdGVkX1@gmail.com>

--- uno.config.ts
+++ uno.config.ts
@@ -2,4 +2,10 @@ import { presetWind3, presetIcons, defineConfig } from "unocss";
 
 export default defineConfig({
   presets: [presetWind3(), presetIcons()],
+  theme: {
+    colors: {
+      // Text emphasis color
+      emphasis: "#002677",
+    },
+  },
 });
