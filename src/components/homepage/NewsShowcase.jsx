9f96702c872ca196b26e9f9dccd2a11e02220791
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-08-28 13:33:30 +0800

    Restore photo and merge upstream changes
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

--- src/components/homepage/NewsShowcase.jsx
+++ src/components/homepage/NewsShowcase.jsx
@@ -12,7 +12,7 @@ const NewsShowcase = () => {
     {
       title: "PLCT实验室携香山南湖笔记本倾情参与BOSC世界RISC-V日",
       description: "2025年8月8日，World RISC-V Days 北京站在北京开源芯片研究院圆满举行。作为由RISC-V 国际基金会推出的首届世界 RISC-V 日盛典，此次活动汇集了来自各地的行业专家、企业代表与开发者分享洞见、交流合作，进一步推动 RISC-V 在中国及全球的广泛落地与持续创新。",
-      img: "img/newsshowcase/xiangshan_process.jpg",
+      img: "img/newsshowcase/xiangshan.jpg",
       link: "https://mp.weixin.qq.com/s/OW8OAXUPN-kWurjVjk2VsQ"
     },
     {
