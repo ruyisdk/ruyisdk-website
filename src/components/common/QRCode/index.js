8bce12f61429432575c94b344335ce0e49d2970e
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-09-02 21:54:36 +0800

    pages: feat the QQgrouplist (#216)
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

---
+++ src/components/common/QRCode/index.js
@@ -0,0 +1,133 @@
+import React from "react";
+import { QRCode as AntdQRCode } from "antd";
+
+const QRCode = ({ 
+  src, // 图片路径
+  value, // 二维码内容（用于动态生成）
+  alt = "QR Code",
+  size = 140,
+  borderRadius = 10,
+  shadow = "0 2px 8px rgba(0,0,0,0.08)",
+  QRnumber = null, // 二维码下方的号码信息
+  style,
+  className,
+  ...props 
+}) => {
+  // 如果提供了src，使用图片
+  if (src) {
+    return (
+      <div 
+        style={{ 
+          textAlign: "left", 
+          margin: "1rem 0", 
+          ...style 
+        }} 
+        className={className}
+        {...props}
+      >
+        <div style={{ 
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center",
+          width: `${size}px`
+        }}>
+          <img
+            src={src}
+            alt={alt}
+            style={{
+              maxWidth: `${size}px`,
+              width: "100%",
+              height: "auto",
+              borderRadius: `${borderRadius}px`,
+              boxShadow: shadow,
+            }}
+          />
+          {QRnumber && (
+            <div
+              style={{
+                marginTop: "0.5rem",
+                fontSize: "0.8rem",
+                color: "#666",
+                textAlign: "center",
+                width: "100%",
+              }}
+            >
+              {QRnumber}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  // 如果提供了value，使用动态生成的二维码
+  if (value) {
+    return (
+      <div 
+        style={{ 
+          textAlign: "left", 
+          margin: "1rem 0", 
+          ...style 
+        }} 
+        className={className}
+        {...props}
+      >
+        <div style={{ 
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center",
+          width: `${size}px`
+        }}>
+          <div
+            style={{
+              display: "inline-block",
+              padding: "8px",
+              borderRadius: `${borderRadius}px`,
+              boxShadow: shadow,
+              backgroundColor: "white",
+            }}
+          >
+            <AntdQRCode 
+              value={value} 
+              size={size - 16} // 减去padding
+              style={{
+                borderRadius: `${borderRadius - 4}px`,
+              }}
+            />
+          </div>
+          {QRnumber && (
+            <div
+              style={{
+                marginTop: "0.5rem",
+                fontSize: "0.8rem",
+                color: "#666",
+                textAlign: "center",
+                width: "100%",
+              }}
+            >
+              {QRnumber}
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  // 如果既没有src也没有value，显示错误信息
+  return (
+    <div 
+      style={{ 
+        textAlign: "left", 
+        margin: "1rem 0",
+        color: "#999",
+        ...style 
+      }} 
+      className={className}
+      {...props}
+    >
+      <p>请提供 src 或 value 属性</p>
+    </div>
+  );
+};
+
+export default QRCode; 
--- src/components/community/index.js
+++ src/components/community/index.js
@@ -1,5 +1,3 @@
 // 社区组件导出
-export { default as QQGroupList } from './QQGroupList';
 export { default as MeetupCards } from './MeetupCards';
-export { default as ButtonSubscription } from './Subscription/ButtonSubscription';
-export { default as WeChatQRCode } from './WeChatQRCode'; 
+export { default as ButtonSubscription } from './Subscription/ButtonSubscription'; 
