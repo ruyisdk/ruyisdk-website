730feb86ecb3be90cec405f52187a08bae885088
LoneBlade-zhang <zizaizhang89@gmail.com>
2025-09-04 00:01:07 +0800

    pages: new contact page QR code group (#223)
    Signed-off-by: LoneBlade-zhang <zizaizhang89@gmail.com>

---
+++ src/components/common/QRCode/Group.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const QRGroup = ({
+  list,
+  children,
+  align = "left", // left | center | right
+  gap = 16,
+  wrap = true,
+  style,
+  className,
+  ...props
+}) => {
+  const justifyMap = { left: "flex-start", center: "center", right: "flex-end" };
+  const resolvedJustifyContent = justifyMap[align] || "flex-start";
+
+  const itemsArray = React.useMemo(() => {
+    if (Array.isArray(list)) return list;
+    if (list) return [list];
+    return React.Children.toArray(children);
+  }, [list, children]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: wrap ? "wrap" : "nowrap",
+        justifyContent: resolvedJustifyContent,
+        alignItems: "flex-start",
+        gap: typeof gap === "number" ? `${gap}px` : gap,
+        paddingLeft: typeof gap === "number" ? `${gap * 3}px` : `calc(${gap} * 3)`,
+        margin: "1rem 0",
+        ...style,
+      }}
+      className={className}
+      {...props}
+    >
+      {itemsArray.map((node, index) => (
+        <div key={index} style={{ display: "flex" }}>
+          {node}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default QRGroup; 
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,4 +2,5 @@
 export { default as CodeBlock } from './docs_utils/CodeBlock';
 export { default as StatisticalData } from './StatisticalData';
 export { default as SlideNews } from './SlideNews';
-export { default as QRCode } from './QRCode'; 
+export { default as QRCode } from './QRCode';
+export { default as QRGroup } from './QRCode/Group.jsx'; 
