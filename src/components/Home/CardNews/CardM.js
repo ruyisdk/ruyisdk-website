2412a7b48c84d8a3985f2668b9743b94b5df53c6
weilinfox <caiweilin@iscas.ac.cn>
2026-03-12 19:23:58 +0800

    pages(index): modify CardNews card size
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- src/components/Home/CardNews/CardM.js
+++ src/components/Home/CardNews/CardM.js
@@ -4,7 +4,7 @@ import cardNews from "./CardNews";
 export default function cardM(card) {
   return (
     <div
-      className="relative overflow-hidden flex cursor-default items-center justify-center transition-transform duration-300 rounded-[10px] shadow-[0_8px_30px_rgba(0,0,0,0.1)] w-full h-auto lg:h-[240px]"
+      className="relative overflow-hidden flex cursor-default items-center justify-center transition-transform duration-300 rounded-[10px] shadow-[0_8px_30px_rgba(0,0,0,0.1)] w-full h-auto lg:aspect-[5/1]"
     >
       {cardNews(card)}
     </div>
--- src/components/Home/CardNews/CardS.js
+++ src/components/Home/CardNews/CardS.js
@@ -8,7 +8,7 @@ export default function cardS(cards) {
         return (
           <div
             key={card.index}
-            className="relative overflow-hidden flex cursor-default items-center justify-center transition-transform duration-300 rounded-[10px] shadow-[0_8px_30px_rgba(0,0,0,0.1)] w-full lg:w-[calc(50%_-_0.5rem)] h-auto lg:h-[240px] lg:flex-none"
+            className="relative overflow-hidden flex cursor-default items-center justify-center transition-transform duration-300 rounded-[10px] shadow-[0_8px_30px_rgba(0,0,0,0.1)] w-full lg:w-[calc(50%_-_0.5rem)] h-auto lg:aspect-[5/2] lg:flex-none"
           >
             {cardNews(card)}
           </div>
