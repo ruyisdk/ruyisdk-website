03cee70048aa6d53d13d5cb80c0b3e35bbe9f8f9
weilinfox <caiweilin@iscas.ac.cn>
2026-05-21 19:02:14 +0800

    pages(blog): remove blog and backup mds
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,16 +62,9 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
         },
-        blog: {
-          blogTitle: "RuyiSDK 博客",
-          blogSidebarTitle: "所有博文",
-          blogSidebarCount: "ALL",
-          showReadingTime: true,
-          blogDescription: "Blog",
-        },
+        blog: false,
         sitemap: {
           ignorePatterns: [
-            '/blog/**',
             '/biweekly/**'
           ]
         },
