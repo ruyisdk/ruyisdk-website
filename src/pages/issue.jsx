276e23b4baa5c96140f8387f63b622db4a0284dd
weilinfox <caiweilin@iscas.ac.cn>
2026-06-23 19:43:54 +0800

    pages(issue): add meta translation for English page
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- src/pages/issue.jsx
+++ src/pages/issue.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { translate } from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
 import Issue from "@site/src/components/Issue";
 
 export default function IssuePage() {
   return (
     <Layout
-      title="Issue"
-      description="RuyiSDK 问题反馈与 issue 提交流程"
+      title={translate({ id: "issue.meta.title", message: "报告问题" })}
+      description={translate({ id: "issue.meta.description", message: "RuyiSDK 问题反馈与 issue 提交流程" })}
     >
       <Issue />
     </Layout>
