9b5e23f4afe8a86d9b7ebb916b7ca08ff108901c
weilinfox <caiweilin@iscas.ac.cn>
2026-08-11 17:49:47 +0800

    Revert some changes
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- src/test/mocks/docusaurusContext.js
+++
@@ -1,6 +0,0 @@
-export default function useDocusaurusContext() {
-  return {
-    siteConfig: {},
-    i18n: { currentLocale: "zh-Hans" },
-  };
-}
--- src/test/mocks/docusaurusTranslate.js
+++
@@ -1,3 +0,0 @@
-export function translate({ message }) {
-  return message;
-}
